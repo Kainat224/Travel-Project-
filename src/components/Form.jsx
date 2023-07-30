8fa49c6a45a6e56a1d147599e05c517c16c29812
Kainat <anmolkainat244@gmail.com>
2023-07-30 07:58:30 -0700

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Form = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3> What do you need for 😍 your trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+        {/* <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option> */}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default Form;
